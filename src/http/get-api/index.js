64655c4c1480bafd56d51a39b083169a9c17ba01
Eduardo Azevedo Grigolo <eduardo.grigolo@frontiercargroup.com>
2020-05-22 23:54:32 +0200

    Initial layout

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -1,12 +1,42 @@
-exports.handler = async function http (req) {
+
+exports.handler = async function http(req) {
+  // var Airtable = require('airtable');
+  // var base = new Airtable({apiKey: 'key3h0orLfvwRORrf'}).base('appJqUbXilt8QG73R');
+
+  // console.log('will fetch')
+
+  // const resposta = base('Fornecedores').select({
+  //   // Selecting the first 3 records in Grid view:
+  //   maxRecords: 3,
+  //   view: "Grid view"
+  // }).eachPage(function page(records, fetchNextPage) {
+  //   // This function (`page`) will get called for each page of records.
+  //   console.log('each page')
+
+  //   records.forEach(function(record) {
+  //     console.log('Retrieved', record.get('Cadastro'));
+  //   });
+
+  //   // To fetch the next page of records, call `fetchNextPage`.
+  //   // If there are more records, `page` will get called again.
+  //   // If there are no more records, `done` will get called.
+  //   fetchNextPage();
+
+  // }, function done(err) {
+  //   if (err) { console.error(err); return; }
+  // });
+
+  // console.log(resposta)
+
   console.log('Begin API called')
+
   return {
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
     body: JSON.stringify({
-      message: 'Hello from your Begin API!'
+      message: resposta
     })
   }
 }
